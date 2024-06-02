c0983d19ccd8e89f1c14d3ba4d514c4f9b0fc198
José Henrique <137122689+josehenriques10@users.noreply.github.com>
2024-06-02 09:40:37 -0300

    Update functions.export.ts

--- src/modules/functions.export.ts
+++ src/modules/functions.export.ts
@@ -1,13 +1,13 @@
-export function range(start: number, stop: number, step = 1) {
+export function range(start: number, stop: number, step: number = 1) {
   const isPositive = step > 0;
-  let numbers = [];
+  let numbers: Array<number> = [];
 
   if (stop === undefined) {
     stop = start;
     start = 0;
   }
 
-  for (let i = start; isPositive ? i < stop : i > stop; i += step) {
+  for (let i: number = start; isPositive ? i < stop : i > stop; i += step) {
     numbers.push(i);
   }
 
